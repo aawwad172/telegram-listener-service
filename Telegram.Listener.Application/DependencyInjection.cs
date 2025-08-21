75e88ccecc6bc1dc348b9a9c9e02241c2e534d75
Ahmad Awwad <aawwad172@gmail.com>
2025-08-21 16:11:27 +0300

    renaming whole project

---
+++ Telegram.Listener.Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Telegram.Listener.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        // Register application services here
+        // Example: services.AddTransient<IMyService, MyService>();
+        return services;
+    }
+}
